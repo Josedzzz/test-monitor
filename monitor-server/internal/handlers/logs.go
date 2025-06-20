534bf7cd547b4d1e60f35508f3d407c5ca17be9a
Joki <josedavidamayar@gmail.com>
2025-06-20 00:12:32 -0500

    feat: capture sender ip and update machine info when receiving logs

--- monitor-server/internal/handlers/logs.go
+++ monitor-server/internal/handlers/logs.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
 
 	"github.com/Josedzzz/monitor-server/internal/models"
 	"github.com/Josedzzz/monitor-server/internal/storage"
 )
 
+// handles incoming log data from a monitor-client instance
 func ReceiveLog(w http.ResponseWriter, r *http.Request) {
 	var payload models.LogPayload
 	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
@@ -15,10 +17,22 @@ func ReceiveLog(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	ip := getIp(r.RemoteAddr)
+	storage.UpdateMachineInfo(payload.VMID, ip, payload.Message)
+
 	if err := storage.SaveLog(payload); err != nil {
 		http.Error(w, "Failed to save log", http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+// extracts the ip address from request
+func getIp(remoteAddr string) string {
+	host, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		return remoteAddr
+	}
+	return host
 }
