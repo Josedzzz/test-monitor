14d52b208848d4246a834a1c6f253fa5a8a1a32b
Joki <josedavidamayar@gmail.com>
2025-06-20 00:17:54 -0500

    add a model for the machine info

---
+++ monitor-server/internal/models/machine_info.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type MachineInfo struct {
+	VMID       string    `json:"vm_id"`
+	IP         string    `json:"ip"`
+	LastSeen   time.Time `json:"last_seen"`
+	Containers []string  `json:"containers,omitempty"`
+}
